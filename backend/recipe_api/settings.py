02d31623bda6b3f22e5a28abc5ff261b60846130
Reshma <u1430631@umail.utah.edu>
2024-07-18 21:19:11 +0000

    Added frontend url for access

--- backend/recipe_api/settings.py
+++ backend/recipe_api/settings.py
@@ -39,6 +39,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
+    "http://192.168.0.29:3000"
     "http://localhost:3000",  # Local frontend URL
     "http://recipe-sharing-website-build.s3-website-us-west-2.amazonaws.com", # S3 frontend URL
     "http://35.89.27.173:8000", # EC2 instance IP
